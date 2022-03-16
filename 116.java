f301ee28e9f20a4e94177c340346dfee1a9b2bed
Álvaro Pimentel <alvaropimentel124@gmail.com>
2022-03-16 10:48:15 +0100

    116

---
+++ 116.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Comp {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int c = s.nextInt();
+        for (int i = 0; i < c; i++) {
+            System.out.println("Hola mundo.");
+        }
+    }
+}
